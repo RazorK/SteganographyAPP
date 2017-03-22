86f89f5e084b4c920ffa1aae99fc98751400f939
Razor King <aimin52qinhao@163.com>
2017-03-22 19:56:31 +0800

    login layout

---
+++ app/src/main/java/com/example/aimin/stegano/activity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.aimin.stegano.activity;
+
+import android.os.Bundle;
+
+import com.example.aimin.stegano.R;
+
+/**
+ * Created by aimin on 2017/3/22.
+ */
+
+public class LoginActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+    }
+}
