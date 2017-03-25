cc36b14d27720ec25a487c583e46bd62614681e8
Razor King <aimin52qinhao@163.com>
2017-03-25 12:37:12 +0800

    add application demo

---
+++ app/src/main/java/com/example/aimin/stegano/LeanApplication_demo.java
@@ -0,0 +1,26 @@
+package com.example.aimin.stegano;
+
+import android.app.Application;
+
+import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.im.v2.AVIMMessageManager;
+import com.avos.avoscloud.im.v2.AVIMTypedMessage;
+
+/**
+ * Created by aimin on 2017/3/22.
+ */
+
+public class LeanApplication_demo extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // 初始化参数依次为 this, AppId, AppKey
+        AVOSCloud.initialize(this,"Your Appid","Your AppKey");
+        AVOSCloud.setDebugLogEnabled(true);
+
+        // 必须在启动的时候注册 MessageHandler
+        // 应用一启动就会重连，服务器会推送离线消息过来，需要 MessageHandler 来处理
+        AVIMMessageManager.registerMessageHandler(AVIMTypedMessage.class, new MessageHandler(this));
+    }
+}
